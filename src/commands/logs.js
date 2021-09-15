7a4971adc675105aa341696a273df8565437fcf5
Vali Malinoiu <0x4139@gmail.com>
2021-09-15 15:31:04 +0300

    feat: adds logs command

--- src/commands/logs.js
+++ src/commands/logs.js
@@ -1,23 +1,28 @@
 const { Command, flags } = require("@oclif/command")
 const AppLogs = require("./app/logs")
+const util = require("../util")
+
 class LogsCommand extends Command {
   async run() {
-    const { flags } = this.parse(LogsCommand)
-    const name = flags.name || "world"
-    this.log(`hello ${name} from /volumes/code/jekyo-cli/src/commands/logs.js`)
+    const git = util.Git(this.config.dataDir)
+    const remote = await git.Remote()
+    if (!remote) {
+      this.error("Application not found, use `jekyo link` to link and existing one or `jekyo create` to create one")
+    }
+    const runes = remote.url.split("/")
+    //quick and dirty
+    const application = runes[runes.length - 1].split(".git")[0]
+    await AppLogs.run(["-n", application])
   }
   async catch(error) {
     util.ErrorHandler(this.error, error)
   }
 }
 
-LogsCommand.description = `Describe the command here
-...
-Extra documentation goes here
-`
+LogsCommand.description = "Retrieves the logs for the current application"
 
 LogsCommand.flags = {
-  name: flags.string({ char: "n", description: "name to print" }),
+  name: flags.string({ char: "n", description: "jekyo application name" }),
 }
 
 module.exports = LogsCommand
