d2e8a09cf65048bc7dedc56a9387948137b0953a
Vali Malinoiu <0x4139@gmail.com>
2021-09-15 15:37:21 +0300

    feat: adds status command

--- src/commands/status.js
+++ src/commands/status.js
@@ -1,20 +1,24 @@
 const { Command, flags } = require("@oclif/command")
-
+const AppStatus = require("./app/status")
+const util = require("../util")
 class StatusCommand extends Command {
   async run() {
-    const { flags } = this.parse(StatusCommand)
-    const name = flags.name || "world"
-    this.log(`hello ${name} from /volumes/code/jekyo-cli/src/commands/status.js`)
+    const git = util.Git(this.config.dataDir)
+    const remote = await git.Remote()
+    if (!remote) {
+      this.error("Application not found, use `jekyo link` to link and existing one or `jekyo create` to create one")
+    }
+    const runes = remote.url.split("/")
+    //quick and dirty
+    const application = runes[runes.length - 1].split(".git")[0]
+    await AppStatus.run(["-n", application])
   }
   async catch(error) {
     util.ErrorHandler(this.error, error)
   }
 }
 
-StatusCommand.description = `Describe the command here
-...
-Extra documentation goes here
-`
+StatusCommand.description = "Retrieves the status for the current application"
 
 StatusCommand.flags = {
   name: flags.string({ char: "n", description: "name to print" }),
